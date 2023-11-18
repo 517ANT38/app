e989fae7583f2122cc02a139be22f1486da73944
517ANT38 <a.tashchilin@bk.ru>
2023-11-18 17:05:45 +0300

    razmetka

--- app_start/start.bash
+++ app_start/start.bash
@@ -1,17 +1,22 @@
 #!/bin/bash
-sudo apt update 
-sudo apt install curl
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
-nvm install node
+
+
 sudo apt update
-sudo apt install postgresql postgresql-contrib
+sudo apt install postgresql postgresql-contrib curl
+
 sudo systemctl start postgresql.service
 sudo su postgres
 psql --file=app_start/create_database.sql
 exit
+# зависимости приложения
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install node
 cd app_marks
 npm install
 npm install pm2 -g
+#запуск приложения
 cd -
 pm2 start -f --name app app_marks/index.js 
+
+
