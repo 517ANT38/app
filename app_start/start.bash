a1c35b996d5d2f7cc230d57046701364a7f9c269
517ANT38 <a.tashchilin@bk.ru>
2023-11-18 16:39:55 +0300

    fix bags

--- app_start/start.bash
+++ app_start/start.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo -n "Пользователь БД $USER ввидите "
-psql -U $USER -W -h "localhost" -p 5432 --file=app_start/create_database.sql
-pm2 start -f --name app /app/app_marks/index.js 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install node
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+sudo systemctl start postgresql.service
+sudo su postgres
+psql  --file=app_start/create_database.sql
+exit
+cd app_marks
+npm install
+npm install pm2 -g
+cd -
+pm2 start -f --name app app_marks/index.js 
