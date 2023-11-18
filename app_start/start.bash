09598530d52ea2f2e46f7219bed776b1b6451c59
517ANT38 <a.tashchilin@bk.ru>
2023-11-19 00:18:29 +0300

    fixs bag

--- app_start/start.bash
+++ app_start/start.bash
@@ -17,9 +17,15 @@ function install_pack {
 }
 netstat -tuln | grep 5432 || install_pack
 #запуск сервера postgres
-sudo -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD 'postgres'"
+sudo -u postgres psql -c "CREATE ROLE myapp LOGIN PASSWORD 'myapp'";
+
 sudo -u postgres psql -c 'CREATE DATABASE appmarks' || echo 'БД appmarks уже существует'; 
 
+sudo -u postgres psql -c 'ALTER DATABASE appmarks OWNER TO myapp';
+
+tp='host   all             myapp             localhost                   md5';
+
+sudo echo $tp >> /etc/postgres/**/main/pg_hba.conf || sudo echo $tp >> /var/lib/pgsql/pg_hba.conf
 #Настройка портов
 sudo firewall-cmd --permanent --add-port=8080/tcp 
 sudo firewall-cmd --permanent --add-port=4567/tcp 
