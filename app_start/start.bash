d210ac2efc46c88f4af553e250ef54404a59f48d
517ANT38 <a.tashchilin@bk.ru>
2023-11-18 17:07:29 +0300

    all linux system

--- app_start/start.bash
+++ app_start/start.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#установка необхомимых пакетов
+packagesNeeded='curl jq postgresql postgresql-contrib'
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt)" ];     then sudo apt update && sudo apt install $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt update && sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+elif [ -x "$(command -v yum)" ];  then sudo yum install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
 
-sudo apt update
-sudo apt install postgresql postgresql-contrib curl
-
+#запуск сервера postgres
 sudo systemctl start postgresql.service
 sudo su postgres
 psql --file=app_start/create_database.sql
