bf01be0c710f782dc5285cb787c42b60867fa5db
517ANT38 <a.tashchilin@bk.ru>
2023-11-18 18:06:40 +0300

    fix bags

--- app_start/start.bash
+++ app_start/start.bash
@@ -14,18 +14,18 @@ else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manual
 sudo systemctl enable postgresql
 sudo systemctl start postgresql.service
 sudo su postgres
-cmdSql='
+cmdSql="
     CREATE EXTENSION IF NOT EXISTS dblink;
     DO
-    $$
+    \$\$
     BEGIN
         IF NOT EXISTS (SELECT datname FROM pg_database WHERE datname='appmarks') THEN
         PERFORM dblink_exec('dbname=' || current_database(),'CREATE DATABASE appmarks');
         END IF;
     END;
-    $$;
+    \$\$;
     DROP EXTENSION dblink;
-'
+"
 psql -c $cmdSql
 exit
 # зависимости приложения
