e76605d2aae1957c0c5443fe7fc7257a6f0a9f3a
517ANT38 <a.tashchilin@bk.ru>
2023-11-18 22:26:33 +0300

    fixs bags

--- app_start/reco_data.bash
+++ app_start/reco_data.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd app_start || echo 'Вы в каталоге app_start'
 sudo mkdir -p /backups
 sudo chown $USER:$USER /backups 
 chmod 0777 /backups
+cd ..
 rsync -aAXv app_marks /backups/
 sudo -u postgres pg_dump -d appmarks > /backups/appmarks.dmp
