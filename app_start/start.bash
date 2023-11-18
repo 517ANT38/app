eeb40f11ed2bb8551e7992d3c4544410f42e3804
517ANT38 <a.tashchilin@bk.ru>
2023-11-18 17:08:10 +0300

    fix bags

--- app_start/start.bash
+++ app_start/start.bash
@@ -4,7 +4,7 @@
 packagesNeeded='curl jq postgresql postgresql-contrib'
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
 elif [ -x "$(command -v apt)" ];     then sudo apt update && sudo apt install $packagesNeeded
-elif [ -x "$(command -v apt-get)" ]; then sudo apt update && sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get update && sudo apt-get install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 elif [ -x "$(command -v yum)" ];  then sudo yum install $packagesNeeded
