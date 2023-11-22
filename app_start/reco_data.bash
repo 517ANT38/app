53d4879d48ac70068dc204e50602f487ec925d5e
517ANT38 <102868848+517ANT38@users.noreply.github.com>
2023-11-22 08:35:28 +0400

    fix bags

--- app_start/reco_data.bash
+++ app_start/reco_data.bash
@@ -4,5 +4,11 @@ sudo mkdir -p /backups
 sudo chown $USER:$USER /backups 
 chmod 0777 /backups
 cd ..
-rsync -aAXv app_marks /backups/
-sudo -u postgres pg_dump -C -d appmarks > /backups/appmarks.dmp
+
+if [ $(pm2 describe app) ] then 
+    echo 'server oneline';
+    exit 1
+else
+    rsync -aAXv app_marks /backups/ || echo 'Service not found'; exit 1
+    sudo -u postgres pg_dump -C -d appmarks > /backups/appmarks.dmp || echo 'Database not found'; exit 1
+fi
