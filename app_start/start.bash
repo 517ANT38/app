55e63cb8667b39ee346f2daa3a051ac25ed945a7
Anton <a.tashchilin@bk.ru>
2023-11-25 21:55:38 +0400

    fxs

--- app_start/start.bash
+++ app_start/start.bash
@@ -26,29 +26,24 @@ function init_db_elem(){
 
     sudo -u postgres psql -c 'ALTER ROLE myapp WITH SUPERUSER';
 }
-#запуск сервера postgres
-function init_dnf_yum(){
-    install_packages 'postgresql-server';
-    sudo postgresql-setup --initdb --unit postgresql
-    sudo systemctl start postgresql;
-    init_db_elem;
-    sudo sed -i "s,#listen_addresses = 'localhost',listen_addresses = '*',g" /var/lib/pgsql/data/postgresql.conf
-    sudo sh -c "echo \"host    all    all    0.0.0.0/0    md5\" >> /var/lib/pgsql/data/pg_hba.conf"
-    sudo sed -i 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
-}
+
 
 # Проверяем, установлен ли пакет postgresql
 if [ -x "$(command -v postgres -V)" ]; then    
     echo "Сервер PostgreSQL установлен."
 else
 
-    if  [ -x "$(command -v dnf)" ] ; then 
-        init_dnf_yum
-    elif [ -x "$(command -v yum)" ] ; then
-        init_dnf_yum
-    else
-        install_packages 'postgresql';
+    if  [ -x "$(command -v dnf || command -v yum)" ] ; then 
+        install_packages 'postgresql-server';
         sudo postgresql-setup --initdb --unit postgresql
+        sudo systemctl start postgresql;
+        init_db_elem;
+        sudo sed -i "s,#listen_addresses = 'localhost',listen_addresses = '*',g" /var/lib/pgsql/data/postgresql.conf
+        sudo sh -c "echo \"host    all    all    0.0.0.0/0    md5\" >> /var/lib/pgsql/data/pg_hba.conf"
+        sudo sed -i 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
+    
+    else
+        install_packages 'postgresql';        
         init_db_elem;
         sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/$(ls /etc/postgresql | awk '{printf $1}')/main/postgresql.conf
         sudo sh -c "echo \"host    all    all    0.0.0.0/0    md5\" >> /etc/postgresql/$(ls /etc/postgresql | awk '{printf $1}')/main/pg_hba.conf"
